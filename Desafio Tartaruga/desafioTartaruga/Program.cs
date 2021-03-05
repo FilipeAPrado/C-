368de52af695a61663cc2413f0d27a337d1019de
FilipeAPrado <filipemaster85@gmail.com>
2021-03-04 23:05:53 -0300

    Commit inicial

---
+++ Desafio Tartaruga/desafioTartaruga/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace desafioTartaruga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contador = 0;
+            Console.WriteLine("Informe o numero de tartarugas");
+          int  numOfTurtles = Int32.Parse(Console.ReadLine());
+            int fastestTurtle = 0;
+
+            int[] velocityTurtles = new int[numOfTurtles];
+            foreach(int cont in velocityTurtles)
+            {
+                Console.WriteLine("qual velocidade da tartaruga : ");
+                velocityTurtles[contador] = Int32.Parse(Console.ReadLine());
+                contador++;
+            }
+            
+            contador = 0;
+            foreach (int velocity in velocityTurtles)
+            {
+                
+                if(fastestTurtle < velocityTurtles[contador])
+                {
+                    fastestTurtle = velocityTurtles[contador];
+                    contador++;
+                }
+            }
+
+            if(fastestTurtle >= 20)
+            {
+                Console.WriteLine(3);
+            }
+            else if(fastestTurtle >= 10 && fastestTurtle <20)
+            {
+                Console.WriteLine(2);
+            }
+            else
+            {
+                Console.WriteLine(1);
+            }
+            Console.Write(fastestTurtle);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Quadrado e ao Cubo/Quadrado e ao Cubo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Quadrado_e_ao_Cubo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = Int32.Parse(Console.ReadLine());
+            int i = 1;
+            while (i <= num)
+            {
+                Console.WriteLine((i * 1) + " " + (i * i) + " " + (i * i) * i);
+                i = i + 1;
+            }
+            Console.ReadLine();
+        }
+    }
+
+}
